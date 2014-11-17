bb08bf77fed6755aedafc4d77562845db109cb33
Jenna Chin <jchin@flycommunications.com>
2014-11-17 12:54:14 -0500

    updated homepage

--- dev/wp-content/themes/html5-boilerplate-for-wordpress-master/front-page.php
+++ dev/wp-content/themes/html5-boilerplate-for-wordpress-master/front-page.php
@@ -12,7 +12,122 @@ get_header(); ?>
     <?php while (have_posts()) : the_post(); ?>
 
       <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-        <?php the_content(); ?>
+      	<?php
+		if ( has_post_thumbnail() ) {
+			$image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID));
+			echo "<section class=\"mtwHero\" style=\"background:url('$image_url[0]') no-repeat;\">";
+		} else {
+			echo "<section class=\"mtwHero\">";
+		}?>
+	        <div class="box">
+            	<article class="heroContent">
+					<?php the_content(); ?>
+                    <div class="heroButtons clearfix">
+                    	<a href="/partners" class="btnMTW">Sign Up Now</a>
+                        <a href="/donate" class="btnDonate">or donate</a>
+                    </div><!--.clearfix-->
+                </article><!--.heroContent-->
+            </div>
+        </section><!--.mtwHero-->
+      
+      	<article class="mtwMission">
+	        <div class="box">
+                <?php
+				$mission = get_post_custom_values('Mission');
+				if(is_array($mission)):
+					foreach ( $mission as $key=> $value ) {
+					  echo $value;					
+					}
+				endif;
+				?>
+            </div>
+        </article><!--.mtwMission-->
+        
+        <section class="coreValues">
+        	<div class="box clearfix">
+          		<h3 class="hdrValues">Our Core Values Include:</h3>
+                <?php $args = array(
+					'category_name' => 'Values',
+					'post_type' => 'post',
+					'order' => 'ASC',
+					'post_status' => 'publish',
+					'numberposts' => 4
+				);
+				$postslist = get_posts($args);
+				foreach ($postslist as $post) :
+					setup_postdata($post);
+				?>
+                <article class="value">
+                	<?php
+                    if ( has_post_thumbnail() ) {
+                        $image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID));
+						echo "<img src=\"$image_url[0]\" alt=\"value icon\" class=\"imgValue\" />";
+                    } else {
+						echo "<div class=\"imgValue\"></div>";
+					}?>
+                    <?php the_content(); ?>
+                </article>
+                <?php endforeach; ?>
+        	</div><!--.box-->
+        </section><!--.coreValues-->
+        
+        <section class="callOuts">
+        	<div class="box clearfix">
+            	<?php $args2 = array(
+					'category_name' => 'Sign-Up',
+					'post_type' => 'post',
+					'order' => 'ASC',
+					'post_status' => 'publish',
+					'numberposts' => 1
+				);
+				$postslist2 = get_posts($args2);
+				foreach ($postslist2 as $post) :
+					setup_postdata($post);
+				?>
+                <?php
+				if ( has_post_thumbnail() ) {
+					$image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID));
+					echo "<article class=\"coSignUp callOutBox\" style=\"background:url('$image_url[0]') no-repeat;\">";
+				} else {
+					echo "<article class=\"coSignUp callOutBox\">";
+				}?>
+                	<article class="coContent">
+                        <h3><?php the_title(); ?></h3>
+                    	<?php the_content(); ?>
+                        <a href="/partners" class="btnCO">Sign Up Now</a>
+                    </article><!--.coContent-->
+	            </article><!--.coSignUp-->
+                <?php endforeach; ?>
+                
+                <?php $args3 = array(
+					'category_name' => 'Tips+Featured',
+					'post_type' => 'post',
+					'order' => 'ASC',
+					'post_status' => 'publish',
+					'numberposts' => 1
+				);
+				$postslist3 = get_posts($args3);
+				foreach ($postslist3 as $post) :
+					setup_postdata($post);
+				?>
+    	        <article class="coTips callOutBox">
+                	<?php
+                    if ( has_post_thumbnail() ) {
+                        $image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID));
+						echo "<img src=\"$image_url[0]\" alt=\"Tip of the Month featured image\" class=\"imgCOTip\" />";
+						echo "<article class=\"coContent hasImg\">";
+                    } else {
+						echo "<article class=\"coContent\">";
+					}?>
+                    	<h3><?php the_title(); ?></h3>
+                    	<?php the_content(); ?>
+                        <a href="<?php the_permalink(); ?>" class="btnCO">Read More</a>
+                    </article><!--.coContent-->
+	            </article><!--.coTips-->
+                <?php endforeach; ?>
+                
+    	    </div><!--.box-->
+        </section><!--.callOuts-->
       </div>
 
     <?php endwhile; ?>
