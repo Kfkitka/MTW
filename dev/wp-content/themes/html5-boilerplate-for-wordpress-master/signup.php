9a212e3890e7ca0f0221b6d5e61debc2cdac6f84
kfkitka <kfkitka@gmail.com>
2014-11-18 16:09:16 -0500

    working on responsiveness on home and signup

--- dev/wp-content/themes/html5-boilerplate-for-wordpress-master/signup.php
+++ dev/wp-content/themes/html5-boilerplate-for-wordpress-master/signup.php
@@ -15,18 +15,28 @@ get_header(); ?>
       	<?php
 		if ( has_post_thumbnail() ) {
 			$image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID));
-			echo "<section class=\"mtwHero\" style=\"background:url('$image_url[0]') no-repeat;\">";
+			echo "<section class=\"signupHero\" style=\"background:url('$image_url[0]') no-repeat;\">";
 		} else {
-			echo "<section class=\"mtwHero\">";
+			echo "<section class=\"signupHero\">";
 		}?>
 	        <div class="box">
-            	<article class="heroContent">
-					<?php the_content(); ?>
-                    <div class="heroButtons clearfix">
-                    	<a href="/partners" class="btnMTW">Sign Up Now</a>
-                        <a href="/donate" class="btnDonate">or donate</a>
-                    </div><!--.clearfix-->
-                </article><!--.heroContent-->
+	        	<article class="signupWrapper clearfix">
+	            	<div class="signupBox">
+						<div class="signupForm">
+						</div>
+	                    <div class="formButton clearfix">
+	                    	<a href="/partners" class="blueSignupBtn">Become a Partner</a>
+	                    </div><!--.clearfix-->
+	                </div>
+	                <div class="signupHeader">
+	                	<p class="signupTitle">
+	                		Pose a question that frames why they should sign up.
+	                	</p>
+	                	<p class="signupSubtitle">
+							Sentence telling them to sign up.
+	                	</p>
+	                </div>
+	            </article>
             </div>
         </section><!--.mtwHero-->
         
@@ -60,14 +70,21 @@ get_header(); ?>
 
         <article class="socialSection">
 	        <div class="box">
-                <?php
-				$proof = get_post_custom_values('Social Proof');
-				if(is_array($proof)):
-					foreach ( $proof as $key=> $value ) {
-					  echo $value;					
-					}
-				endif;
+                <?php $args3 = array(
+					'category_name' => 'social-proof',
+					'post_type' => 'post',
+					'order' => 'ASC',
+					'post_status' => 'publish',
+					'numberposts' => 1
+				);
+				$postslist3 = get_posts($args3);
+				foreach ($postslist3 as $post) :
+					setup_postdata($post);
 				?>
+                	<article class="socialContent">
+                    	<?php the_content(); ?>
+                    </article>
+                <?php endforeach; ?>
 				<div class="greenSignupBtn">
                 	<a href="/partners" class="bigBtn">Join the Movement</a>
                 </div>
